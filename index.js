c8b45ff7631e2a6f4e9733e7717e25bf5425edc1
Balram Rathore <balram0698@gmail.com>
2021-03-20 20:05:51 +0530

    new event added

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require("express");
 var http = require("http");
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
+var io = require("socket.io")(server);
 
 //middlewre
 app.use(express.json());
-app.use(cors());
 
-io.on("Connection", (socket) => {
+io.on("connection", (socket) => {
   console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("/test", (msg) => {
+    console.log(msg);
+  });
 });
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log("server started");
 });
