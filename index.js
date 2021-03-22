f1380a2e66bf5dd51b761112a83161c0b0312260
Balram Rathore <balram0698@gmail.com>
2021-03-22 10:40:18 +0530

    client sign in

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ var io = require("socket.io")(server);
 
 //middlewre
 app.use(express.json());
+var clients = {};
 
 io.on("connection", (socket) => {
   console.log("connetetd");
   console.log(socket.id, "has joined");
-  socket.on("/test", (msg) => {
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
     console.log(msg);
   });
 });
